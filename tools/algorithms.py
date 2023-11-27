c83afa205335b7299612ddf5ffecc9a4ca312fbc
Anton Almqvist <jantonalmqvist@gmail.com>
2023-11-27 21:37:13 +0100

    Added function to perform dijkstras
    Extended the tool library with a dijkstras function to find the
    distance of the shortest path between two nodes in a connected graph.
    
    Also made a minor improvement to the function that parses a file line
    by line to not include any empty lines, along with adding a gitignore
    file.

---
+++ tools/algorithms.py
@@ -0,0 +1,28 @@
+import heapq
+import numpy as np
+
+def dijkstras(start, end, adj_mat):
+    '''
+    Performs dijkstra's algorithm on the provided graph to find distance of the
+    shortest path between start and end. If there is no path between start and
+    end then -1 is returned.
+
+    Parameters
+    start: The starting node index
+    end: The ending node index
+    adj_mat: The adjecancy matrix for the graph (NxN np.ndarray)
+    '''
+    if adj_mat.shape[0] != adj_mat.shape[1]:
+        raise ValueError(f"Adjacency matrix is not squared ({adj_mat.shape}")
+    N = adj_mat.shape[0]
+    h = [(0, start)]
+    visited = np.zeros((N))
+    visited[start] = 1
+    while len(h) > 0:
+        dist, curr = heapq.heappop(h)
+        if curr == end:
+            return dist
+        for i in range(N):
+            if visited[i] == 0 and adj_mat[curr, i] != 0:
+                heapq.heappush(h, (dist+adj_mat[curr, i], i))
+    return -1
--- tools/parsing.py
+++ tools/parsing.py
@@ -1,6 +1,10 @@
-def read_by_line(filepath: str, format_func=None):
+def read_by_line(filepath: str, parse_func=None):
     with open(filepath, "r") as f:
+        # read lines and replace line break and clear line characters
         out = [x.replace('\n', '').replace('\r', '') for x in f.readlines()]
-        if format_func is not None:
-            out = [format_func(x) for x in out]
+        # remove any empty lines
+        out = [x for x in out if x is not '']
+        # if each line should be parsed into something else than a string
+        if parse_func is not None:
+            out = [parse_func(x) for x in out]
         return out
