b4475fe652c72cd24ef5bc337fb8f4dad5e17ef7
Anton Almqvist <jantonalmqvist@gmail.com>
2023-12-18 20:36:48 +0100

    Fast solution for day 18
    Only calculate rows without horizontal edges once

--- day18/main.py
+++ day18/main.py
@@ -30,39 +30,57 @@ def digOut(inst: list[int,int]) -> tuple[list[int,int,int],list[int,int,int]]:
     return vert, horiz
 
 def fill(vert: list[int,int,int], horiz: list[int,int,int]):
-    y_min, y_max  = min([v[1] for v in vert]), max([v[2] for v in vert])
-    x_min, x_max = min([v[0] for v in vert]), max([v[0] for v in vert])
     filled = 0
-    y = y_min
-    print("Numbers to look through to look through:", y_max-y_min)
-    for y in range(y_min, y_max+1):
-        xs = [v for v in vert if y >= v[1] and y <= v[2]]
+
+    # List of y-values with horizontal edges for smarter solution
+    break_points = list(set([x[0] for x in horiz]))
+    break_points.sort()
+
+    # Only calculate specific rows where we have new values
+    for row in range(len(break_points)):
+        y1 = break_points[row]
+        xs = [v for v in vert if y1 >= v[1] and y1 <= v[2]]
         xs.sort()
-        h = [(hx[1], hx[2]) for hx in horiz if hx[0] == y]
-        i = 0
-        # Start outside the area
-        inside = False
-        for i in range(len(xs)-1):
-            filled += 1
-            x1, x2 = xs[i], xs[i+1]
-            between = x2[0]-x1[0]-1
-            # On horizontal line, add 
-            if (x1[0], x2[0]) in h:
-                filled += between
-                # Flip to flip back on next turn
-                if x2[3] != x1[3]:
-                    inside = not inside
-                continue
-            # On an edge
-            inside = not inside
-            if inside:
-                filled += between
-        filled += 1
-        if (y-y_min) % 1000000 == 1:
-            print("Another 1000000 done", (y-y_min) // 1000000)
+        h = [(hx[1], hx[2]) for hx in horiz if hx[0] == y1]
+        area_row = fill_row(xs, h)
+        filled += area_row
+        # If we have any rows without horizontal edges between
+        # Find that area and multiply by number of rows
+        if row+1 < len(break_points) and break_points[row+1] > y1+1:
+            y2 = break_points[row] + 1
+            xs = [v for v in vert if y2 > v[1] and y2 < v[2]]
+            xs.sort()
+            h = [(hx[1], hx[2]) for hx in horiz if hx[0] == y2]
+            area_row = fill_row(xs, h)
+            n_rows = break_points[row+1]-y1-1
+            filled += area_row*n_rows
+
     return filled
 
-def decodeHex(col: str) -> tuple[str, int]:
+def fill_row(xs, h):
+    filled = 0
+    i = 0
+    # Start outside the area
+    inside = False
+    for i in range(len(xs)-1):
+        filled += 1
+        x1, x2 = xs[i], xs[i+1]
+        between = x2[0]-x1[0]-1
+        # On horizontal line, add 
+        if (x1[0], x2[0]) in h:
+            filled += between
+            # Flip to flip back on next turn
+            if x2[3] != x1[3]:
+                inside = not inside
+            continue
+        # On an edge
+        inside = not inside
+        if inside:
+            filled += between
+    filled += 1
+    return filled
+
+def decode_hex(col: str) -> tuple[str, int]:
     dist = int(col[:-1], 16)
     match int(col[-1]):
         case 0:
@@ -80,7 +98,7 @@ def run():
     vert, horiz = digOut([(x,y) for x, y, _ in inst])
     p1 = fill(vert, horiz)
     print(p1)
-    vert, horiz = digOut([decodeHex(col) for _, _, col in inst])
+    vert, horiz = digOut([decode_hex(col) for _, _, col in inst])
     p2 = fill(vert, horiz)
     print(p2)
 
